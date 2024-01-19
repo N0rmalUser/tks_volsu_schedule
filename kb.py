9f3e7a457cd4aea6cec4772ec04c4b92936016e8
N0rmalUser <rsk-211_814795@volsu.ru>
2024-01-19 14:21:45 +0300

    added new teachers

--- kb.py
+++ kb.py
@@ -27,8 +27,6 @@ rooms = [
     ], [
         InlineKeyboardButton(text="3-15К", callback_data="room-3-15k"),
         InlineKeyboardButton(text="3-16К", callback_data="room-3-16k")
-    ], [
-        InlineKeyboardButton(text="добавлю позже", callback_data="ignore")
     ]
 ]
 groups = [
@@ -54,52 +52,45 @@ groups = [
 teachers = [
     [
         InlineKeyboardButton(text="Арепьева Е.Е.", callback_data="teacher-Арепьева Е.Е."),
-        InlineKeyboardButton(text="Безбожнов О.Н.", callback_data="teacher-Безбожнов О.Н.")
+        InlineKeyboardButton(text="Безбожнов О.Н.", callback_data="teacher-Безбожнов О.Н."),
     ], [
         InlineKeyboardButton(text="Василюк Д.И.", callback_data="teacher-Василюк Д.И."),
-        InlineKeyboardButton(text="Галич С.В.", callback_data="teacher-Галич С.В.")
+        InlineKeyboardButton(text="Галич С.В.", callback_data="teacher-Галич С.В."),
     ], [
+        InlineKeyboardButton(text="Гладков И.Ф.", callback_data="teacher-Гладков И.Ф."),
         InlineKeyboardButton(text="Гомазкова Л.К.", callback_data="teacher-Гомазкова Л.К."),
-        InlineKeyboardButton(text="Дмитриенко Н.Р.", callback_data="teacher-Дмитриенко Н.Р.")
     ], [
+        InlineKeyboardButton(text="Дмитриенко Н.Р.", callback_data="teacher-Дмитриенко Н.Р."),
         InlineKeyboardButton(text="Елфимова О.И.", callback_data="teacher-Елфимова О.И."),
-        InlineKeyboardButton(text="Ермакова Н.Н.", callback_data="teacher-Ермакова Н.Н.")
     ], [
+        InlineKeyboardButton(text="Ермакова Н.Н.", callback_data="teacher-Ермакова Н.Н."),
         InlineKeyboardButton(text="Задорожнева Ю.В.", callback_data="teacher-Задорожнева Ю.В."),
-        InlineKeyboardButton(text="Карташевский В.Г.", callback_data="teacher-Карташевский В.Г.")
+    ], [
+        InlineKeyboardButton(text="Карташевский В.Г.", callback_data="teacher-Карташевский В.Г."),
+        InlineKeyboardButton(text="Керенцева Н.Д.", callback_data="teacher-Керенцева Н.Д."),
     ], [
         InlineKeyboardButton(text="Пономарев И.Н.", callback_data="teacher-Пономарев И.Н."),
-        InlineKeyboardButton(text="Ромасевич Е.П.", callback_data="teacher-Ромасевич Е.П.")
+        InlineKeyboardButton(text="Ромасевич Е.П.", callback_data="teacher-Ромасевич Е.П."),
     ], [
         InlineKeyboardButton(text="Ромасевич П.В.", callback_data="teacher-Ромасевич П.В."),
-        InlineKeyboardButton(text="Сафонова О.Е.", callback_data="teacher-Сафонова О.Е.")
+        InlineKeyboardButton(text="Сафонова О.Е.", callback_data="teacher-Сафонова О.Е."),
     ], [
         InlineKeyboardButton(text="Семенов Е.С.", callback_data="teacher-Семенов Е.С."),
-        InlineKeyboardButton(text="Стебенькова Н.А.", callback_data="teacher-Стебенькова Н.А.")
+        InlineKeyboardButton(text="Семенова О.В.", callback_data="teacher-Семенова О.В."),
     ], [
+        InlineKeyboardButton(text="Серёженко И.Д.", callback_data="teacher-Серёженко И.Д."),
+        InlineKeyboardButton(text="Стебенькова Н.А.", callback_data="teacher-Стебенькова Н.А."),
+    ], [
+        InlineKeyboardButton(text="Трофимов А.И.", callback_data="teacher-Трофимов А.И."),
         InlineKeyboardButton(text="Тюхтяев Д.А.", callback_data="teacher-Тюхтяев Д.А."),
-        InlineKeyboardButton(text="Стебеньков А.М.", callback_data="teacher-Стебеньков А.М.")
     ], [
+        InlineKeyboardButton(text="Стебеньков А.М.", callback_data="teacher-Стебеньков А.М."),
         InlineKeyboardButton(text="Чадаев Д.И.", callback_data="teacher-Чадаев Д.И."),
-        InlineKeyboardButton(text="Черных С.В.", callback_data="teacher-Черных С.В.")
+    ], [
+        InlineKeyboardButton(text="Черных С.В.", callback_data="teacher-Черных С.В."),
     ]
 ]
 
-group_week0 = [
-    [
-        InlineKeyboardButton(text="Пн", callback_data="day/group/0/1"),
-        InlineKeyboardButton(text="Вт", callback_data="day/group/0/2"),
-        InlineKeyboardButton(text="Ср", callback_data="day/group/0/3")
-    ], [
-        InlineKeyboardButton(text="Чт", callback_data="day/group/0/4"),
-        InlineKeyboardButton(text="Пт", callback_data="day/group/0/5"),
-        InlineKeyboardButton(text="Сб", callback_data="day/group/0/6")
-    ],
-    [
-        InlineKeyboardButton(text="✅ Числитель", callback_data="ignore"),
-        InlineKeyboardButton(text="Знаменатель ➡️", callback_data="week-group-1")
-    ]
-]
 group_week1 = [
     [
         InlineKeyboardButton(text="Пн", callback_data="day/group/1/1"),
@@ -111,26 +102,26 @@ group_week1 = [
         InlineKeyboardButton(text="Сб", callback_data="day/group/1/6")
     ],
     [
+        InlineKeyboardButton(text="✅ Числитель", callback_data="ignore"),
+        InlineKeyboardButton(text="Знаменатель ➡️", callback_data="week-group-2")
+    ]
+]
+group_week2 = [
+    [
+        InlineKeyboardButton(text="Пн", callback_data="day/group/2/1"),
+        InlineKeyboardButton(text="Вт", callback_data="day/group/2/2"),
+        InlineKeyboardButton(text="Ср", callback_data="day/group/2/3")
+    ], [
+        InlineKeyboardButton(text="Чт", callback_data="day/group/2/4"),
+        InlineKeyboardButton(text="Пт", callback_data="day/group/2/5"),
+        InlineKeyboardButton(text="Сб", callback_data="day/group/2/6")
+    ],
+    [
         InlineKeyboardButton(text="✅ Знаменатель", callback_data="ignore"),
-        InlineKeyboardButton(text="Числитель ➡️", callback_data="week-group-0")
+        InlineKeyboardButton(text="Числитель ➡️", callback_data="week-group-1")
     ]
 ]
 
-teacher_week0 = [
-    [
-        InlineKeyboardButton(text="Пн", callback_data="day/teacher/0/1"),
-        InlineKeyboardButton(text="Вт", callback_data="day/teacher/0/2"),
-        InlineKeyboardButton(text="Ср", callback_data="day/teacher/0/3")
-    ], [
-        InlineKeyboardButton(text="Чт", callback_data="day/teacher/0/4"),
-        InlineKeyboardButton(text="Пт", callback_data="day/teacher/0/5"),
-        InlineKeyboardButton(text="Сб", callback_data="day/teacher/0/6")
-    ],
-    [
-        InlineKeyboardButton(text="✅ Числитель", callback_data="ignore"),
-        InlineKeyboardButton(text="Знаменатель ➡️", callback_data="week-teacher-1")
-    ]
-]
 teacher_week1 = [
     [
         InlineKeyboardButton(text="Пн", callback_data="day/teacher/1/1"),
@@ -142,23 +133,23 @@ teacher_week1 = [
         InlineKeyboardButton(text="Сб", callback_data="day/teacher/1/6")
     ],
     [
-        InlineKeyboardButton(text="✅ Знаменатель", callback_data="ignore"),
-        InlineKeyboardButton(text="Числитель ➡️", callback_data="week-teacher-0")
+        InlineKeyboardButton(text="✅ Числитель", callback_data="ignore"),
+        InlineKeyboardButton(text="Знаменатель ➡️", callback_data="week-teacher-2")
     ]
 ]
-room_week0 = [
+teacher_week2 = [
     [
-        InlineKeyboardButton(text="Пн", callback_data="day/room/0/1"),
-        InlineKeyboardButton(text="Вт", callback_data="day/room/0/2"),
-        InlineKeyboardButton(text="Ср", callback_data="day/room/0/3")
+        InlineKeyboardButton(text="Пн", callback_data="day/teacher/2/1"),
+        InlineKeyboardButton(text="Вт", callback_data="day/teacher/2/2"),
+        InlineKeyboardButton(text="Ср", callback_data="day/teacher/2/3")
     ], [
-        InlineKeyboardButton(text="Чт", callback_data="day/room/0/4"),
-        InlineKeyboardButton(text="Пт", callback_data="day/room/0/5"),
-        InlineKeyboardButton(text="Сб", callback_data="day/room/0/6")
+        InlineKeyboardButton(text="Чт", callback_data="day/teacher/2/4"),
+        InlineKeyboardButton(text="Пт", callback_data="day/teacher/2/5"),
+        InlineKeyboardButton(text="Сб", callback_data="day/teacher/2/6")
     ],
     [
-        InlineKeyboardButton(text="✅ Числитель", callback_data="ignore"),
-        InlineKeyboardButton(text="Знаменатель ➡️", callback_data="week-room-1")
+        InlineKeyboardButton(text="✅ Знаменатель", callback_data="ignore"),
+        InlineKeyboardButton(text="Числитель ➡️", callback_data="week-teacher-1")
     ]
 ]
 room_week1 = [
@@ -170,9 +161,24 @@ room_week1 = [
         InlineKeyboardButton(text="Чт", callback_data="day/room/1/4"),
         InlineKeyboardButton(text="Пт", callback_data="day/room/1/5"),
         InlineKeyboardButton(text="Сб", callback_data="day/room/1/6")
+    ],
+    [
+        InlineKeyboardButton(text="✅ Числитель", callback_data="ignore"),
+        InlineKeyboardButton(text="Знаменатель ➡️", callback_data="week-room-2")
+    ]
+]
+room_week2 = [
+    [
+        InlineKeyboardButton(text="Пн", callback_data="day/room/2/1"),
+        InlineKeyboardButton(text="Вт", callback_data="day/room/2/2"),
+        InlineKeyboardButton(text="Ср", callback_data="day/room/2/3")
+    ], [
+        InlineKeyboardButton(text="Чт", callback_data="day/room/2/4"),
+        InlineKeyboardButton(text="Пт", callback_data="day/room/2/5"),
+        InlineKeyboardButton(text="Сб", callback_data="day/room/2/6")
     ], [
         InlineKeyboardButton(text="✅ Знаменатель", callback_data="ignore"),
-        InlineKeyboardButton(text="Числитель ➡️", callback_data="week-room-0")
+        InlineKeyboardButton(text="Числитель ➡️", callback_data="week-room-1")
     ]
 ]
 view = [
@@ -183,14 +189,14 @@ view = [
     ]
 ]
 
-group_week0 = InlineKeyboardMarkup(inline_keyboard=group_week0)
 group_week1 = InlineKeyboardMarkup(inline_keyboard=group_week1)
+group_week2 = InlineKeyboardMarkup(inline_keyboard=group_week2)
 
-teacher_week0 = InlineKeyboardMarkup(inline_keyboard=teacher_week0)
 teacher_week1 = InlineKeyboardMarkup(inline_keyboard=teacher_week1)
+teacher_week2 = InlineKeyboardMarkup(inline_keyboard=teacher_week2)
 
-room_week0 = InlineKeyboardMarkup(inline_keyboard=room_week0)
 room_week1 = InlineKeyboardMarkup(inline_keyboard=room_week1)
+room_week2 = InlineKeyboardMarkup(inline_keyboard=room_week2)
 
 rooms = InlineKeyboardMarkup(inline_keyboard=rooms)
 groups = InlineKeyboardMarkup(inline_keyboard=groups)
