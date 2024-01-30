1bacfb1384bdcd8aa57d8a9cbbc05f0443254cc6
N0rmalUser <rsk-211_814795@volsu.ru>
2024-01-30 21:09:26 +0300

    added log file path

--- data/config.py
+++ data/config.py
@@ -1,17 +1,18 @@
-# BOT_TOKEN = "6310285588:AAGFIo5qQz8UMhZ4NWFTIJ5ic0Yeochyu2I"
-BOT_TOKEN = "6857740783:AAF2JsAWEkLra0u9PdWHBUFySonglckn_Vk"
+# BOT_TOKEN = "6345780201:AAEdrv7OPmEYFYbOnjW8taXELLmKfksfF7M"
+BOT_TOKEN = "6469830886:AAF9QhbENGAs7w9mH0x_BJ-a-o2lPTTgDOA"
 # ID админской группы в которую будут приходить сообщения о юзеров
-ADMIN_CHAT_ID = -1001991471871
-
+# ADMIN_CHAT_ID = -1001991471871
+ADMIN_CHAT_ID = -1001966122887
+LOG_FILE = 'bot.log'
 
 # Время задержки для спама (Сколько должно пройти времени между сообщениями для того, чтобы не считаться спамом)
 THROTTLE_TIME = 1
 
-# Путь до папки с исходниками расписаний
-ORIGINAL_SCHEDULES_PATH = "C:\\Users\\normal\\Desktop\\tks\\preparation\\"
 # Путь до базы данных
 DB_PATH = "data\\users.db"
 # Путь до файла с расписанием в формате json
 SCHEDULE_PATH = "data\\combined_schedule.json"
+# Путь до папки с исходниками расписаний
+ORIGINAL_SCHEDULES_PATH = "C:\\Users\\normal\\Desktop\\tks\\preparation\\"
 
 schedule = None
