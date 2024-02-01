5cf71c4033a32e9fb847a96b06e0a720c3df6c46
N0rmalUser <rsk-211_814795@volsu.ru>
2024-02-01 16:25:30 +0300

    подготовил к работе бота

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-# BOT_TOKEN = "6345780201:AAEdrv7OPmEYFYbOnjW8taXELLmKfksfF7M"
-BOT_TOKEN = "6469830886:AAF9QhbENGAs7w9mH0x_BJ-a-o2lPTTgDOA"
+BOT_TOKEN = "6345780201:AAEdrv7OPmEYFYbOnjW8taXELLmKfksfF7M"
+# BOT_TOKEN = "6469830886:AAF9QhbENGAs7w9mH0x_BJ-a-o2lPTTgDOA"
+
 # ID админской группы в которую будут приходить сообщения о юзеров
-# ADMIN_CHAT_ID = -1001991471871
-ADMIN_CHAT_ID = -1001966122887
+ADMIN_CHAT_ID = -1001991471871
+# ADMIN_CHAT_ID = -1001966122887
+
 LOG_FILE = 'logs/bot.log'
 
 # Время задержки для спама (Сколько должно пройти времени между сообщениями для того, чтобы не считаться спамом)
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import asyncio
 
 from bot import bot, database as db
-
+from config import LOG_FILE
 import logging
 
 
 if __name__ == "__main__":
     db.init_db()
     db.open_schedule_file()
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s",
+    logging.basicConfig(level=logging.INFO, filename=LOG_FILE, format="%(asctime)s %(levelname)s %(message)s",
                         datefmt='%H:%M:%S %d-%m-%Y', encoding="utf-8")
     logging.getLogger('aiogram.event').setLevel(logging.WARNING)
     asyncio.run(bot.main())
-# , filename=LOG_FILE]
