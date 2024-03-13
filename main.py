f0c7e5b0e6f0ae9c98a01b23caaa40eee153c35a
N0rmalUser <rsk-211_814795@volsu.ru>
2024-03-13 21:00:37 +0300

    удалил ненужную функцию
    Signed-off-by: N0rmalUser <rsk-211_814795@volsu.ru>

--- main.py
+++ main.py
@@ -4,20 +4,6 @@ import config
 import logging
 
 
-# def extract_and_sort_group_names(file_path):
-#     conn = sqlite3.connect(config.SCHEDULE_PATH)
-#     cursor = conn.cursor()
-#     cursor.execute("""
-#         SELECT DISTINCT GroupName
-#         FROM Groups
-#         GROUP BY GroupName
-#         """)
-#     group_names = cursor.fetchall()
-#     groups = [row[0] for row in group_names]
-#
-#     return sorted(groups)
-
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, filename=config.LOG_FILE, format="%(asctime)s %(levelname)s %(message)s",
                         datefmt='%H:%M:%S %d-%m-%Y', encoding="utf-8")
