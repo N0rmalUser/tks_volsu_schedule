00c379704bd2efde17054467e55b7540a8756a84
normal <rsk-211_814795@volsu.ru>
2024-06-17 15:30:07 +0300

    Сделал удаление просроченных данных во вспомогательной таблице. Сделал отслеживание активности в рамках часа.

--- bot/database/activity.py
+++ bot/database/activity.py
@@ -1,3 +1,5 @@
+import logging
+
 from config import ACTIVITIES_DB, USERS_DB, timezone
 
 from datetime import datetime, timedelta
@@ -11,52 +13,60 @@ import sqlite3
 
 @sql_kit(USERS_DB)
 def get_last_hour_users(cursor: sqlite3.Cursor) -> list:
-    """Возвращает список пользователей, которые заходили в бота в последний час"""
-    one_hour_ago = datetime.now(pytz.timezone(timezone)) - timedelta(hours=1)
+    """Возвращает список пользователей, которые были активны в течение предыдущего часа."""
 
-    cursor.execute("SELECT user_id, last_date FROM User_Info")
-    all_users = cursor.fetchall()
+    start_of_last_hour = datetime.now(pytz.timezone(timezone)).replace(minute=0, second=0, microsecond=0)- timedelta(hours=1)
+    end_of_last_hour = start_of_last_hour + timedelta(hours=1)
 
-    active_users = []
-    for user_id, last_date_str in all_users:
-        last_date = pytz.timezone(timezone).localize(datetime.strptime(last_date_str, '%d-%m-%Y %H:%M:%S'))
-        if last_date >= one_hour_ago:
-            active_users.append(user_id)
-    return active_users
+    cursor.execute("""
+        SELECT user_id FROM User_Info
+        WHERE last_date >= ? AND last_date < ?
+    """, (start_of_last_hour.strftime('%d-%m-%Y %H:%M:%S'), end_of_last_hour.strftime('%d-%m-%Y %H:%M:%S')))
+
+    return [row[0] for row in cursor.fetchall()]
 
 
 @sql_kit(ACTIVITIES_DB)
 def update_user_activity_stats(cursor: sqlite3.Cursor):
+    """Обновляет статистику активности пользователей за последний час. Удаляет данные из вспомогательной таблицы User_Unique_Activity, которые устарели на 2 часа."""
+
+    two_hours_ago = datetime.now(pytz.timezone(timezone)).replace(minute=0, second=0, microsecond=0) - timedelta(hours=2)
+
+    cursor.execute("""
+        DELETE FROM User_Unique_Activity
+        WHERE date < ? OR (date = ? AND hour < ?)
+        """, (two_hours_ago.strftime('%d-%m-%Y'), two_hours_ago.strftime('%d-%m-%Y'), two_hours_ago.hour))
+
     active_users = get_last_hour_users()
     current_datetime = datetime.now(pytz.timezone('Europe/Moscow'))
     date_str = current_datetime.strftime('%d-%m-%Y')
     current_hour = current_datetime.hour
 
-    # Сначала обновляем таблицу уникальных пользователей
-    for user_id in active_users:
-        cursor.execute("""
-               INSERT OR IGNORE INTO User_Unique_Activity (date, hour, user_id)
-               VALUES (?, ?, ?)
-           """, (date_str, current_hour, user_id))
+    cursor.executemany("""
+        INSERT OR IGNORE INTO User_Unique_Activity (date, hour, user_id)
+        VALUES (?, ?, ?)
+    """, [(date_str, current_hour, user_id) for user_id in active_users])
 
-    # Теперь подсчитываем количество уникальных пользователей для данного часа
     cursor.execute("""
-           SELECT COUNT(*) FROM User_Unique_Activity
-           WHERE date = ? AND hour = ?
+       SELECT COUNT(*) FROM User_Unique_Activity
+       WHERE date = ? AND hour = ?
        """, (date_str, current_hour))
 
     user_count = cursor.fetchone()[0]
 
-    # Обновляем основную таблицу статистики активности пользователей
     cursor.execute("""
-           INSERT INTO User_Activity_Stats (date, hour, user_count)
-           VALUES (?, ?, ?)
-           ON CONFLICT(date, hour) DO UPDATE SET user_count = excluded.user_count
+       INSERT INTO User_Activity_Stats (date, hour, user_count)
+       VALUES (?, ?, ?)
+       ON CONFLICT(date, hour) DO UPDATE SET user_count = excluded.user_count
        """, (date_str, current_hour, user_count))
+
+    logging.info(f"Статистика активности пользователей за {current_datetime.strftime('%d-%m-%Y %H:%M')} обновлена")
 
 
 @sql_kit(ACTIVITIES_DB)
 def fetch_user_activity_stats(cursor: sqlite3.Cursor):
+    """Возвращает статистику активности пользователей за последние 24 часа."""
+
     cursor.execute("SELECT * FROM User_Activity_Stats")
     data = cursor.fetchall()
     return data
