4f3d27973c99c889e93941342fd4e401dc80ec60
normal <rsk-211_814795@volsu.ru>
2024-06-17 15:31:39 +0300

    добавил автоматический сбор данных активности

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 import asyncio
 from bot import bot
 from bot.database.db_init import db_init
 from config import LOG_FILE, timezone
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import pytz
+
+
+def start_scheduler():
+    """Запускает планировщик задач, который обновляет статистику активности пользователей каждый час."""
+
+    from bot.database.activity import update_user_activity_stats
+
+    scheduler = BackgroundScheduler()
+    next_hour = (datetime.now(pytz.timezone(timezone)) + timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+    scheduler.add_job(update_user_activity_stats, 'interval', hours=1, start_date=next_hour)
+    scheduler.start()
 
 
 if __name__ == "__main__":
@@ -13,4 +25,5 @@ if __name__ == "__main__":
                         datefmt='%H:%M:%S %d-%m-%Y', encoding="utf-8")
     logging.getLogger('aiogram.event').setLevel(logging.WARNING)
     db_init()
+    start_scheduler()
     asyncio.run(bot.main())
