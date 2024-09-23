10b2915504ebf64482c6bb27668954e06b229873
N0rmalUser <vova.okunev25@gmail.com>
2024-09-23 12:16:30 +0300

    Изменил цвета и улучшил даты

--- app/misc/user_activity.py
+++ app/misc/user_activity.py
@@ -13,12 +13,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+from datetime import datetime
 from math import ceil
 
 import pandas as pd
 from lets_plot import *
-from pandas import DataFrame
 
 
 def plot_activity_for_day(df: pd.DataFrame, date: str):
@@ -26,43 +25,48 @@ def plot_activity_for_day(df: pd.DataFrame, date: str):
     df["Hour"] = pd.to_datetime(df["Hour"], format="%Y-%m-%d %H:%M").dt.strftime("%H")
 
     y_max = ceil(df["User Count"].max() / 10) * 10
-    background = element_rect(fill="#14181e")
     (
         ggplot(df, aes(x="Hour", y="User Count"))
         + ggsize(1000, 500)
-        + geom_bar(stat="identity", fill="blue")
+        + geom_bar(stat="identity", color="#cae6ff", fill="#cae6ff")
         + ggtitle(f"Активность пользователей по часам на {date}")
         + xlab("Время")
         + ylab("Количество пользователей")
-        + scale_y_continuous(limits=[0, y_max], breaks=list(range(0, y_max + 1, 10)))
+        + scale_y_continuous(
+            limits=[0, y_max], breaks=list(range(0, y_max + 1, 1 if y_max == 10 else 5))
+        )
         + flavor_high_contrast_dark()
         + theme(
+            axis_text_x=element_text(angle=0, hjust=0.5),
             panel_grid_major_x="blank",
-            plot_background=background,
+            text=element_text(color="#d3e5f5"),
+            plot_background=element_rect(fill="#122634"),
             plot_title=element_text(),
         )
     ).to_html("data/activity_for_day.html")
 
 
-def plot_activity_for_month(df: DataFrame, month: str):
+def plot_activity_for_month(df: pd.DataFrame, month: str):
     LetsPlot.setup_html(no_js=True)
     df["Date"] = pd.to_datetime(df["Date"], format="%Y-%m-%d")
 
     y_max = ceil(df["User Count"].max() / 10) * 10
-    background = element_rect(fill="#14181e")
     (
         ggplot(df, aes(x="Date", y="User Count"))
         + ggsize(1000, 500)
-        + geom_bar(stat="identity", fill="blue")
-        + ggtitle(f"Активность пользователей по дням  {month}")
+        + geom_bar(stat="identity", color="#cae6ff", fill="#cae6ff")
+        + ggtitle(f"Активность пользователей по дням с {month}")
         + xlab("Дата")
         + ylab("Количество пользователей")
-        + scale_y_continuous(limits=[0, y_max], breaks=list(range(0, y_max + 1, 10)))
+        + scale_y_continuous(
+            limits=[0, y_max], breaks=list(range(0, y_max + 1, 1 if y_max == 10 else 5))
+        )
         + flavor_high_contrast_dark()
         + theme(
-            axis_text_x=element_text(angle=70, hjust=1),
+            axis_text_x=element_text(angle=0, hjust=0.5),
             panel_grid_major_x="blank",
-            plot_background=background,
+            text=element_text(color="#d3e5f5"),
+            plot_background=element_rect(fill="#122634"),
             plot_title=element_text(),
         )
     ).to_html("data/activity_for_month.html")
