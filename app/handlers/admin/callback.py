0b2f953d5da28ac84c2a9b040050e3430f0c1026
N0rmalUser <vova.okunev25@gmail.com>
2025-09-18 16:48:44 +0300

    Удалил лишние импорты

--- app/handlers/admin/callback.py
+++ app/handlers/admin/callback.py
@@ -13,8 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-import asyncio
-import re
 
 from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
