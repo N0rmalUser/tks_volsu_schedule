88321c9b8ea736a9db1a5a0c25d0b9d347452925
N0rmalUser <vova.okunev25@gmail.com>
2026-08-23 22:55:31 +0300

    Перенёс в тг и убрал неиспользуемое состояние

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -22,7 +22,7 @@ from pathlib import Path
 
 from dateutil.relativedelta import relativedelta
 
-from app.config import ACTIVITIES_DB, SCHEDULE_DB, USERS_DB, VK_DB
+from app.config import USERS_DB
 from app.database.user import User
 
 
@@ -49,134 +49,6 @@ def sql_kit(db: Path = ":memory:"):
 
     return decorator
 
-
-@sql_kit(USERS_DB)
-def user_db_init(cursor: sqlite3.Cursor) -> None:
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS User_Info (
-            user_id INTEGER PRIMARY KEY,
-            user_type TEXT DEFAULT 'student',
-            topic_id INTEGER,
-            start_date TIMESTAMP,
-            last_date TIMESTAMP,
-            blocked BOOLEAN DEFAULT false,
-            banned BOOLEAN DEFAULT false,
-            default_choose TEXT
-        )
-    """,
-    )
-
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS Temp_Data (
-            user_id INTEGER PRIMARY KEY,
-            tracking BOOLEAN DEFAULT false,
-            teacher_id INTEGER DEFAULT 0,
-            group_id INTEGER DEFAULT 0,
-            FOREIGN KEY (user_id) REFERENCES User_Info(user_id)
-        )
-    """,
-    )
-
-
-@sql_kit(VK_DB)
-def vk_user_db_init(cursor: sqlite3.Cursor) -> None:
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS Vk_User_Info (
-            user_id INTEGER PRIMARY KEY,
-            user_type TEXT DEFAULT 'student',
-            start_date TIMESTAMP,
-            last_date TIMESTAMP,
-            teacher_id INTEGER DEFAULT 0,
-            group_id INTEGER DEFAULT 0
-        )
-    """,
-    )
-
-
-@sql_kit(SCHEDULE_DB)
-def schedule_db_init(cursor: sqlite3.Cursor) -> None:
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS Rooms (
-            RoomID INTEGER PRIMARY KEY AUTOINCREMENT,
-            RoomName TEXT UNIQUE NOT NULL)""",
-    )
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS Groups (
-            GroupID INTEGER PRIMARY KEY AUTOINCREMENT,
-            GroupName TEXT UNIQUE NOT NULL)""",
-    )
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS Teachers (
-            TeacherID INTEGER PRIMARY KEY AUTOINCREMENT,
-            TeacherName TEXT UNIQUE NOT NULL)""",
-    )
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS Subjects (
-            SubjectID INTEGER PRIMARY KEY AUTOINCREMENT,
-            SubjectName TEXT UNIQUE NOT NULL)""",
-    )
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS Schedule (
-            ScheduleID INTEGER PRIMARY KEY AUTOINCREMENT,
-            Time TEXT NOT NULL,
-            DayOfWeek TEXT NOT NULL,
-            WeekType TEXT NOT NULL,
-            GroupID INTEGER,
-            Subgroup INTEGER,
-            TeacherID INTEGER,
-            RoomID INTEGER,
-            SubjectID INTEGER,
-            FOREIGN KEY (GroupID) REFERENCES Groups(GroupID),
-            FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID),
-            FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID),
-            FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))""",
-    )
-
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS CollegeSchedule (
-            ScheduleID INTEGER PRIMARY KEY AUTOINCREMENT,
-            Time TEXT NOT NULL,
-            DayOfWeek TEXT NOT NULL,
-            WeekType TEXT NOT NULL,
-            GroupID INTEGER,
-            TeacherID INTEGER,
-            RoomID INTEGER,
-            SubjectID INTEGER,
-            FOREIGN KEY (GroupID) REFERENCES Groups(GroupID),
-            FOREIGN KEY (TeacherID) REFERENCES Teachers(TeacherID),
-            FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID),
-            FOREIGN KEY (SubjectID) REFERENCES Subjects(SubjectID))""",
-    )
-
-
-@sql_kit(ACTIVITIES_DB)
-def activity_db_init(cursor: sqlite3.Cursor) -> None:
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS daily_activity (
-            user_ids TEXT,
-            date TEXT,
-            PRIMARY KEY (user_ids, date)
-        );
-    """,
-    )
-
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS hourly_activity (
-            user_ids TEXT,
-            datetime TEXT,
-            PRIMARY KEY (user_ids, datetime)
-        );
-    """,
-    )
 
 
 def format_date(date: str) -> str:
@@ -368,13 +240,3 @@ def get_users_by_teacher_id(group_id: int, cursor: sqlite3.Cursor) -> str | None
     )
     return row[0] if (row := cursor.fetchone()) else None
 
-
-try:
-    user_db_init()
-    vk_user_db_init()
-    schedule_db_init()
-    activity_db_init()
-    logging.info("Базы данных инициализированы")
-except Exception as e:
-    logging.error(f"Ошибка в инициализации базы данных: {e}")
-    raise e
